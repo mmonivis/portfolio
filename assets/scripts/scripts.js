23e433d2bd78e18251e9884a874e2b980bda20ec
Marissa Monivis <mmonivis@gmail.com>
2017-07-19 09:41:28 -0400

    Added temp page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,56 +6,56 @@ $(document).ready(function(){
 	    $('.nav-links').toggle();
 	});
 
-	(function($) {
+	// (function($) {
 
-	  /**
-	   * Copyright 2012, Digital Fusion
-	   * Licensed under the MIT license.
-	   * http://teamdf.com/jquery-plugins/license/
-	   *
-	   * @author Sam Sehnert
-	   * @desc A small plugin that checks whether elements are within
-	   *     the user visible viewport of a web browser.
-	   *     only accounts for vertical position, not horizontal.
-	   */
+	//   /**
+	//    * Copyright 2012, Digital Fusion
+	//    * Licensed under the MIT license.
+	//    * http://teamdf.com/jquery-plugins/license/
+	//    *
+	//    * @author Sam Sehnert
+	//    * @desc A small plugin that checks whether elements are within
+	//    *     the user visible viewport of a web browser.
+	//    *     only accounts for vertical position, not horizontal.
+	//    */
 
-	  $.fn.visible = function(partial) {
+	//   $.fn.visible = function(partial) {
 	    
-	      var $t            = $(this),
-	          $w            = $(window),
-	          viewTop       = $w.scrollTop(),
-	          viewBottom    = viewTop + $w.height(),
-	          _top          = $t.offset().top,
-	          _bottom       = _top + $t.height(),
-	          compareTop    = partial === true ? _bottom : _top,
-	          compareBottom = partial === true ? _top : _bottom;
+	//       var $t            = $(this),
+	//           $w            = $(window),
+	//           viewTop       = $w.scrollTop(),
+	//           viewBottom    = viewTop + $w.height(),
+	//           _top          = $t.offset().top,
+	//           _bottom       = _top + $t.height(),
+	//           compareTop    = partial === true ? _bottom : _top,
+	//           compareBottom = partial === true ? _top : _bottom;
 	    
-	    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+	//     return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-	  };
+	//   };
 	    
-	})(jQuery);
+	// })(jQuery);
 
-	var win = $(window);
+	// var win = $(window);
 
-	var allMods = $(".example");
+	// var allMods = $(".example");
 
-	allMods.each(function(i, el) {
-	  var el = $(el);
-	  if (el.visible(true)) {
-	    el.addClass("already-visible"); 
-	  } 
-	});
+	// allMods.each(function(i, el) {
+	//   var el = $(el);
+	//   if (el.visible(true)) {
+	//     el.addClass("already-visible"); 
+	//   } 
+	// });
 
-	win.scroll(function(event) {
+	// win.scroll(function(event) {
 	  
-	  allMods.each(function(i, el) {
-	    var el = $(el);
-	    if (el.visible(true)) {
-	      el.addClass("come-in"); 
-	    } 
-	  });
+	//   allMods.each(function(i, el) {
+	//     var el = $(el);
+	//     if (el.visible(true)) {
+	//       el.addClass("come-in"); 
+	//     } 
+	//   });
 	  
-	});
+	// });
 
 });
